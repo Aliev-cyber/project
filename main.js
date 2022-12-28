819efed97c91c9ef3e09092416975c82d4dce053
Said Aliev <user@userui-MacBookPro.local>
2022-12-28 19:28:43 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,275 @@
+//  Переменные//  строгий режим"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj ={
+//     a:50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+
+                            //правила и типы название       
+    // const vehicleBodyWidth = 5000;
+    // const vehicleBodyLenght = 4000;
+
+    // console.log("Ширина кузова автомобиля:" + vehicleBodyWidth + ',длина:' + vehicleBodyLenght);
+
+
+                   // Home work//
+
+// const userName = 'Said'
+// let userNumber = 20;
+// userNumber = 21;
+
+
+                            //простые типы 
+    // let number =4.6;
+    // console.log(-4/0);
+    // console.log('string'*9);
+
+
+
+
+
+
+
+
+
+
+                            //обьекты
+
+    // const obj = {
+    //     name:"John",
+    //     age:25,
+    //     isMarried: false
+    // }
+    // // console.log(obj.name);
+    // console.log(obj["name"]);
+
+    // let arr = ['plum.png','orange.png',6,'apple.bmp',{},[]];
+    // console.log(arr[1]);
+
+
+
+                    //home work 
+
+
+//      let storeName='said'
+
+// let storeDescriotion ={
+//     budget:1000,
+//     employees:['john','Alihan','Said'],
+//     products:{
+//         'limon':20,
+//         'apple':30
+//     },
+//     open:true
+// }
+
+
+    // alert('Hello');
+
+    // const result = confirm ("Are you here?");
+    // console.log(result);
+
+
+    // const answer = prompt("Вам есть 18?","18");
+    // console.log(answer + 5);
+
+
+
+                                   // Инторполяция (ЕS6)
+
+
+        // const category = 'toys';
+
+        // console.log(`https://someurl.com/${category}/5`);
+
+        // const user ="Ivan";
+
+        // alert(`Привет, ${user}`);
+
+
+                                //операторы в Js
+
+        // console.log('arr'+"- object");
+        // console.log(4+"5");
+
+        // let incr = 10,
+        // decr =10;
+
+        // ++incr;
+        // --decr; 
+        // console.log(++incr);
+        // console.log(--decr);
+
+        // console.log(5%2);
+
+        // console.log(2+2*2 !=='6');
+
+        // const isChecked = false,
+        // isClose = false;
+        // console.log(isChecked ||  !isClose);
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const UL = document.querySelectorALL('ul>li>span');
+
+// document.querySelector('table').onclick = function (even){
+//     if (even.target.tagName !== 'TD') return false;
+//     // console.log(event.target);
+//     let data = [...event.target.parentNode.children];
+//     let text = getDataFromTd(data);
+//     // console.log(text);
+//     outText(text);
+// }
+
+// function getDataFromTd(TdArr){
+//     return TdArr.map(item => item.textContent);
+// }
+
+// function outText(text){
+//     text.forEach((item,index)=> UL[index].textContent = item);
+// }
+
+
+
+
+
+// const div = document.createElement('div');
+// const header = document.createElement('h1');
+// header .innerText = 'Learn JS'
+
+// div.appendChild(header)
+// document.body.appendChild(div)
+// console.log(div)
+// console.log(header)
+
+
+
+// const div  = document.createElement('div')
+// const header = document.createElement('h1')
+// header .innerText = 'Said Krasavchik'
+
+// div.appendChild(header)
+// document.body.appendChild(div)
+// console.log(div)
+// console.log(header)
+
+
+// const div = document.createElement('div')
+// const header = document.createElement('h1')
+// header.innerText = 'good like'
+// header .classList.add('test')
+// const a = document.createElement('a');
+// a.href = 'https://www.youtube.com/watch?v=5wBgfKvUpVQ'
+// a.innerText = 'MDN'
+
+
+// div.appendChild(header)
+// div.appendChild(a)
+// document.body.appendChild(div)
+// console.log(div)
+// console.log(a)
+
+
+
+
+
+
+// console.log(one.geTAttribute('data'));
+// console.log(document.querySelectorALL('link')[1].geTAttribute('href'));
+
+
+// one.seTAttribute('data-num',6);
+
+
+// let gas =document.querySelectorALL('.gas');
+// for (let i = 0; i < gas.length; i++) {
+//     gas[i].onclick = function(){
+//         let gallons = document.querySelector('.gallons').value;
+//         let amount = this.geTAttribute('data');
+//         console.log(gallons * amount);
+//     }
+// }
+
+// let a = document.createElement('div');
+// a.innerHTML = 'Hello!';
+// a.classList.add('one');
+// a.onclick = function (){
+//     alert('hello');
+// }
+
+// document.querySelector('.test').appendChild(a);
+
+
+// console.log(a);
+
+
+const da = 'Mir';
